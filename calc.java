57518dd64d0ab633984d1b6a07da4cd07da22c0b
nick93201 <nm.nikhil@iiitb.ac.in>
2024-01-09 11:48:45 +0530

    added by team2

--- calc.java
+++ calc.java
@@ -1,6 +1,6 @@
 public class helloworld {
 	public static void main (String [] args ) { 
 		System.out.println("hello world");
-		System.out.println("hello world,program modified by team1");
+		System.out.println("hello world,program modified by team2");
 		System.exit( 0);
 }}
